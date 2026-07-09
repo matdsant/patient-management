ff960a068ddbedb18744646ea1d98b328729c7e7
Matheus dos Santos <matheus.dso1996@gmail.com>
2026-07-09 16:01:02 -0300

    Configured Protobuf / GRPC in the patient-service

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -5,16 +5,21 @@ import billing.BillingRequest;
 import billing.BillingResponse;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
 
-    @Override
-    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
-        // TODO: implementar a lógica real de criação da conta de faturamento
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received {}", billingRequest.toString());
         BillingResponse response = BillingResponse.newBuilder()
-                .setAccountId("123")
+                .setAccountId("12345")
                 .setStatus("ACTIVE")
                 .build();
 
